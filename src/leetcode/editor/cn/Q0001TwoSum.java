5f0f75fb994b41737d89ed5c47d4a58a1d6f2544
燎原 <2240141419@qq.com>
2022-10-09 22:12:31 +0800

    Initial commit

---
+++ src/leetcode/editor/cn/Q0001TwoSum.java
@@ -0,0 +1,18 @@
+package leetcode.editor.cn;
+/** 
+    * @author  燎原 
+    * @date    2022-10-09 22:09:09 
+*/
+public class Q0001TwoSum{  
+     public static void main(String[] args) {
+             Solution solution = new Q0001TwoSum().new Solution();    
+     }    
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+}
